1569e0c49eeba0afc3f7516e3f16f634d15bec4e
Jordi Irawan <jordirwn@gmail.com>
2022-07-08 20:52:49 +0700

    add Get user

--- src/handlers/users.handler.js
+++ src/handlers/users.handler.js
@@ -5,4 +5,9 @@ module.exports = {
     const userRes = await Users.updateOne({ _id: userId }, request.payload);
     return h.response({ updated: Boolean(userRes.modifiedCount) });
   },
+  getUser: async (request, h) => {
+    const userId = request.auth.credentials.userId;
+    const user = await Users.findById(userId).select('-password -_id');
+    return h.response(user);
+  },
 };
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -4,6 +4,28 @@ const userHandler = require('../handlers/users.handler');
 module.exports = [
   {
     path: '/users',
+    method: 'GET',
+    handler: userHandler.getUser,
+    options: {
+      tags: ['api'],
+      description: 'Get user',
+      validate: {
+        headers: Joi.object({
+          authorization: Joi.string().required().description('Bearer YOUR_TOKEN'),
+        }).unknown(),
+      },
+      response: {
+        schema: Joi.object({
+          email: Joi.string().email(),
+          photo: Joi.string().uri(),
+          name: Joi.string(),
+          title: Joi.string(),
+        }).unknown(),
+      },
+    },
+  },
+  {
+    path: '/users',
     method: 'PUT',
     handler: userHandler.updateProfile,
     options: {
